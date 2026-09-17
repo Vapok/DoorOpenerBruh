cd92a246ff278918459f56744752a4578aab3685
Vapok <vapokrocks@gmail.com>
2026-09-17 05:06:05 -0400

    Release v2.0.5: Jewelcrafting Font Compatibility

--- DoorOpenerBruh/DoorOpenerBruh.cs
+++ DoorOpenerBruh/DoorOpenerBruh.cs
@@ -26,7 +26,7 @@ namespace DoorOpenerBruh
         //Module Constants
         private const string _pluginId = "vapok.mods.dooropenerbruh";
         private const string _displayName = "DoorOpenerBruh";
-        private const string _version = "2.0.4";
+        private const string _version = "2.0.5";
         
         //Interface Properties
         public string PluginId => _pluginId;
